f392fe8a343f8f5a12a9aa05d085d28e97229d79
Coded <64589027+CodedCA@users.noreply.github.com>
2022-01-09 16:00:32 -0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,73 +1,83 @@
-particlesJS("particles-container", {
-    particles: {
-      number: { value: 150, density: { enable: true, value_area: 800 } },
-      color: { value: "#ffffff" },
-      shape: {
-        type: "triangle",
-        stroke: { width: 0, color: "#000000" },
-        polygon: { nb_sides: 5 },
-        image: { src: "img/github.svg", width: 100, height: 100 }
-      },
-      opacity: {
-        value: 0.5,
-        random: false,
-        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
-      },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1
-      },
-      move: {
-        enable: true,
-        speed: 6,
-        direction: "none",
-        random: false,
-        straight: false,
-        out_mode: "out",
-        bounce: false,
-        attract: { enable: false, rotateX: 600, rotateY: 1200 }
-      }
-    },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: { enable: true, mode: "repulse" },
-        onclick: { enable: true, mode: "push" },
-        resize: true
-      },
-      modes: {
-        grab: { distance: 400, line_linked: { opacity: 1 } },
-        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-        repulse: { distance: 200, duration: 0.4 },
-        push: { particles_nb: 4 },
-        remove: { particles_nb: 2 }
-      }
-    },
-    retina_detect: true
-  });
-  var count_particles, stats, update;
-  stats = new Stats();
-  stats.setMode(0);
-  stats.domElement.style.position = "absolute";
-  stats.domElement.style.left = "0px";
-  stats.domElement.style.top = "0px";
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector(".js-count-particles");
-  update = function () {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
+var myVar;
+var check = 0;
+var active = 0;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 500);
+  document.getElementById("body").classList="tran";
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("mostrar").style.display = "block";
+}
+
+function change() {
+  if (check === 0) {
+    document.getElementById("body").style.background="#B2BECD";
+    document.getElementById("moon-icon").classList="far fa-moon";
+    document.getElementById("text-main").style.color="#111518";
+    document.getElementById("text-sub").style.color="#242d33";
+    document.getElementById("text-sub-x").style.color="#dadada";
+    document.getElementById("square").style.borderColor="#dadada";
+    document.getElementById("linea").style.borderTop="3px solid #dadada";
+    document.getElementById("dark").style.color="#242d33";
+    document.getElementById("white").style.color="#B2BECD";
+    document.getElementById("whitex").style.color="#111518";
+    document.getElementById("py").style.color="#242d33";
+    document.getElementById("css").style.color="#242d33";
+    document.getElementById("html").style.color="#242d33";
+    document.getElementById("js").style.color="#242d33";
+    document.getElementById("git").style.color="#dadada";
+    document.getElementById("env").style.color="#dadada";
+    document.getElementById("inst").style.color="#dadada";
+    document.getElementById("twi").style.color="#dadada";
+    document.getElementById("hamburger-icon").style.color="#242d33";
+    check = 1;
+  }
+  else if (check === 1) {
+    document.getElementById("body").style.background="#111518";
+    document.getElementById("hamburger-icon").style.color="#B2BECD";
+    document.getElementById("moon-icon").classList="fas fa-moon";
+    document.getElementById("text-main").style.color="#dadada";
+    document.getElementById("text-sub").style.color="#B2BECD";
+    document.getElementById("text-sub-x").style.color="#dadada";
+    document.getElementById("linea").style.borderTop="3px solid #242d33";
+    document.getElementById("square").style.borderColor="#242d33";
+    document.getElementById("dark").style.color="#B2BECD";
+    document.getElementById("white").style.color="#111518";
+    document.getElementById("whitex").style.color="#B2BECD";
+    document.getElementById("py").style.color="#B2BECD";
+    document.getElementById("css").style.color="#B2BECD";
+    document.getElementById("html").style.color="#B2BECD";
+    document.getElementById("js").style.color="#B2BECD";
+    document.getElementById("git").style.color="#242d33";
+    document.getElementById("env").style.color="#242d33";
+    document.getElementById("inst").style.color="#242d33";
+    document.getElementById("twi").style.color="#242d33";
+    
+    check = 0;
+  }
+}
+
+function hamburger() {
   
+
+  if (active === 0) {
+    document.getElementById("menu").style.visibility="visible";
+    document.getElementById("menu").style.overflow="visible";
+    document.getElementById("menu-txt").classList="shown";
+    document.getElementById("hamburger-icon").style.position="fixed";
+    document.getElementById("mostrar").style.display = "none";
+    active = 1;
+  }
+  else if (active === 1) {
+    document.getElementById("menu").style.visibility="hidden";
+    document.getElementById("menu").style.overflow="hidden";
+    document.getElementById("menu-txt").classList="hidden";
+    document.getElementById("hamburger-icon").style.position="absolute";
+    document.getElementById("mostrar").style.display = "block";
+    active = 0;
+  }
+}
+
