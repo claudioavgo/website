2f24d98277c163c449cd39bb88389fa63aba6b2d
Coded <64589027+CodedCA@users.noreply.github.com>
2022-01-11 02:59:14 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+var myVar;
+var r = document.querySelector(':root');
+var actived = 0;
+
+
+function loader() {
+  myVar = setTimeout(showPage, 500);
+}
+
+function showPage() {
+  document.getElementById("loading").style.display = "block";
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("loading-clear").style.display = "none";
+}
+
+function darkmode() {
+    if (actived === 0) {
+        document.getElementById("dark-mode-btn").classList="btn-menu-darkmode far fa-moon";
+        r.style.setProperty('--main', '#dadada');
+        r.style.setProperty('--color-light', '#111518');
+        r.style.setProperty('--color-gray', '#242d33');
+        r.style.setProperty('--color-dark', '#B2BECD');
+        actived = 1;
+    }
+    else if (actived === 1) {
+        document.getElementById("dark-mode-btn").classList="btn-menu-darkmode fas fa-moon";
+        r.style.setProperty('--main', '#111518');
+        r.style.setProperty('--color-light', '#dadada');
+        r.style.setProperty('--color-gray', '#B2BECD');
+        r.style.setProperty('--color-dark', '#242d33');
+        actived = 0;
+    }
+}
+
+document.addEventListener("");
