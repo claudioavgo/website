fd148086f49d6d3ed1cbbf1fd792fbb581a614bc
Coded <64589027+CodedCA@users.noreply.github.com>
2022-01-11 02:56:17 -0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,83 +1,35 @@
 var myVar;
-var check = 0;
-var active = 0;
+var r = document.querySelector(':root');
+var actived = 0;
 
-function myFunction() {
+
+function loader() {
   myVar = setTimeout(showPage, 500);
-  document.getElementById("body").classList="tran";
 }
 
 function showPage() {
+  document.getElementById("loading").style.display = "block";
   document.getElementById("loader").style.display = "none";
-  document.getElementById("mostrar").style.display = "block";
+  document.getElementById("loading-clear").style.display = "none";
 }
 
-function change() {
-  if (check === 0) {
-    document.getElementById("body").style.background="#B2BECD";
-    document.getElementById("moon-icon").classList="far fa-moon";
-    document.getElementById("text-main").style.color="#111518";
-    document.getElementById("text-sub").style.color="#242d33";
-    document.getElementById("text-sub-x").style.color="#dadada";
-    document.getElementById("square").style.borderColor="#dadada";
-    document.getElementById("linea").style.borderTop="3px solid #dadada";
-    document.getElementById("dark").style.color="#242d33";
-    document.getElementById("white").style.color="#B2BECD";
-    document.getElementById("whitex").style.color="#111518";
-    document.getElementById("py").style.color="#242d33";
-    document.getElementById("css").style.color="#242d33";
-    document.getElementById("html").style.color="#242d33";
-    document.getElementById("js").style.color="#242d33";
-    document.getElementById("git").style.color="#dadada";
-    document.getElementById("env").style.color="#dadada";
-    document.getElementById("inst").style.color="#dadada";
-    document.getElementById("twi").style.color="#dadada";
-    document.getElementById("hamburger-icon").style.color="#242d33";
-    check = 1;
-  }
-  else if (check === 1) {
-    document.getElementById("body").style.background="#111518";
-    document.getElementById("hamburger-icon").style.color="#B2BECD";
-    document.getElementById("moon-icon").classList="fas fa-moon";
-    document.getElementById("text-main").style.color="#dadada";
-    document.getElementById("text-sub").style.color="#B2BECD";
-    document.getElementById("text-sub-x").style.color="#dadada";
-    document.getElementById("linea").style.borderTop="3px solid #242d33";
-    document.getElementById("square").style.borderColor="#242d33";
-    document.getElementById("dark").style.color="#B2BECD";
-    document.getElementById("white").style.color="#111518";
-    document.getElementById("whitex").style.color="#B2BECD";
-    document.getElementById("py").style.color="#B2BECD";
-    document.getElementById("css").style.color="#B2BECD";
-    document.getElementById("html").style.color="#B2BECD";
-    document.getElementById("js").style.color="#B2BECD";
-    document.getElementById("git").style.color="#242d33";
-    document.getElementById("env").style.color="#242d33";
-    document.getElementById("inst").style.color="#242d33";
-    document.getElementById("twi").style.color="#242d33";
-    
-    check = 0;
-  }
+function darkmode() {
+    if (actived === 0) {
+        document.getElementById("dark-mode-btn").classList="btn-menu-darkmode far fa-moon";
+        r.style.setProperty('--main', '#dadada');
+        r.style.setProperty('--color-light', '#111518');
+        r.style.setProperty('--color-gray', '#242d33');
+        r.style.setProperty('--color-dark', '#B2BECD');
+        actived = 1;
+    }
+    else if (actived === 1) {
+        document.getElementById("dark-mode-btn").classList="btn-menu-darkmode fas fa-moon";
+        r.style.setProperty('--main', '#111518');
+        r.style.setProperty('--color-light', '#dadada');
+        r.style.setProperty('--color-gray', '#B2BECD');
+        r.style.setProperty('--color-dark', '#242d33');
+        actived = 0;
+    }
 }
 
-function hamburger() {
-  
-
-  if (active === 0) {
-    document.getElementById("menu").style.visibility="visible";
-    document.getElementById("menu").style.overflow="visible";
-    document.getElementById("menu-txt").classList="shown";
-    document.getElementById("hamburger-icon").style.position="fixed";
-    document.getElementById("mostrar").style.display = "none";
-    active = 1;
-  }
-  else if (active === 1) {
-    document.getElementById("menu").style.visibility="hidden";
-    document.getElementById("menu").style.overflow="hidden";
-    document.getElementById("menu-txt").classList="hidden";
-    document.getElementById("hamburger-icon").style.position="absolute";
-    document.getElementById("mostrar").style.display = "block";
-    active = 0;
-  }
-}
-
+document.addEventListener("");
